64c96014782b45f5d6800d354170223314770ddf
Rexashh <Rexashh@users.noreply.github.com>
2022-05-04 22:24:49 +0000

    Automated code formatting
    Signed-off-by: kennedy-ex <kennedy-ex@users.noreply.github.com>

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -11,14 +11,20 @@ from EmikoRobot import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/2d22dc1fec0273ad7a0a2.jpg"
 
+
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lord Famous Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [ʀᴇxᴧ-ᴇx](https://t.me/JustRex)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/lordfamousassistantbot?start=help"), Button.url("Support", "https://t.me/famouspublic")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lord Famous Robot.** \n\n"
+    TEXT += "⚪ **I'm Working Properly** \n\n"
+    TEXT += f"⚪ **My Master : [ʀᴇxᴧ-ᴇx](https://t.me/JustRex)** \n\n"
+    TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+    TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+    TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+    TEXT += "**Thanks For Adding Me Here ❤️**"
+    BUTTON = [
+        [
+            Button.url("Help", "https://t.me/lordfamousassistantbot?start=help"),
+            Button.url("Support", "https://t.me/famouspublic"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
